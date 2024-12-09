4bed326ea8d1419bf43ee458f3eb571a843afdd9
mohamed-farouk07 <60257984+mohamed-farouk07@users.noreply.github.com>
2024-12-09 17:57:55 +0200

    test

--- medicova/src/components/Layout/SideBar/SideBar.tsx
+++ medicova/src/components/Layout/SideBar/SideBar.tsx
@@ -34,9 +34,9 @@ const SideBar = ({ isOpen }: { isOpen?: boolean }) => {
   const toggleCvSearch = () => setOpenCvSearch(!openCvSearch);
 
   const classMobileOpen =
-    "fixed top-[100px] left-0 z-40 h-[calc(100vh-100px)] translate-x-0 bg-[#f7f7fd] overflow-hidden duration-300 w-60 ease-in-out shadow-2xl text-black";
+    "fixed top-[100px] left-0 z-40 h-[calc(100vh-100px)] translate-x-0 bg-[#f7f7fd] overflow-hidden duration-300 w-60 ease-in-out shadow-2xl text-[#7C8493CC]";
   const classNormal =
-    "fixed top-[100px] left-0 translate-x-[-100%] md:translate-x-0 z-40 h-[calc(100vh-100px)] bg-[#f7f7fd] overflow-hidden w-14 text-black duration-300 ease-in-out hover:w-60 md:block lg:sticky lg:w-60 shadow-2xl";
+    "fixed top-[100px] left-0 translate-x-[-100%] md:translate-x-0 z-40 h-[calc(100vh-100px)] bg-[#f7f7fd] overflow-hidden w-14 text-[#7C8493CC] duration-300 ease-in-out hover:w-60 md:block lg:sticky lg:w-60 shadow-2xl";
   return (
     <Box className={isOpen ? classMobileOpen : classNormal}>
       <Box className="lg:mt-0 w-60 overflow-y-auto max-h-[calc(100vh-100px)] overflow-x-hidden pt-5 scroll-bar-hidden">
